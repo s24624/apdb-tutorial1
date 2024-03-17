ea27fd2f64f9513b946861a9e9dbcf7f17207e6b
s24624 <s24624@pjwstk.edu.pl>
2024-03-18 00:27:52 +0100

    Dodano metode, ktora zwraca srednia z tablicy integerow

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,17 @@
         int a = 12;
         Console.WriteLine(a*2);
         int[] tab = { 1, 2, 10, 13, 2, 6 };
+        Console.WriteLine(average(tab));
         Console.WriteLine(featureMax(tab));
+        
+    }
+
+    public static double average(int[] tab)
+    {
+        int sum = 0;
+        for (int i = 0; i < tab.Length; i++)
+            sum += tab[i];
+        return sum / tab.Length;
     }
 
     public static int featureMax(int[] tab)
