bef54c72eec2a3c57f43b7339a862da6c5dc1ae2
s24624 <s24624@pjwstk.edu.pl>
2024-03-18 00:33:26 +0100

    zmiana nazwy zmiennej sum

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,10 +13,10 @@
 
     public static double average(int[] tab)
     {
-        int sum = 0;
+        int sum1 = 0;
         for (int i = 0; i < tab.Length; i++)
-            sum += tab[i];
-        return sum / tab.Length;
+            sum1 += tab[i];
+        return sum1 / tab.Length;
     }
 
     public static int featureMax(int[] tab)
