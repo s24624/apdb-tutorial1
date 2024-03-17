4d6abe09d99b9c13b95634e15a8852c0da75662e
s24624 <s24624@pjwstk.edu.pl>
2024-03-18 00:14:17 +0100

    Dodano metode ktora zwraca maksymalna liczbe z tablicy int

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,5 +5,19 @@
         Console.WriteLine("start");
         int a = 12;
         Console.WriteLine(a*2);
+        int[] tab = { 1, 2, 10, 13, 2, 6 };
+        Console.WriteLine(featureMax(tab));
+    }
+
+    public static int featureMax(int[] tab)
+    {
+        int max = tab[0];
+        for (int i = 0; i < tab.Length; i++)
+        {
+            if (tab[i] > max)
+                max = tab[i];
+        }
+
+        return max;
     }
 }
